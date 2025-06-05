d8783df7d9184e3036731b757ec64e87cd88eb6a
Adithkumarrai <adithrai423@gmail.com>
2025-06-05 22:05:14 +0600

    Personal

--- config/db.js
+++ config/db.js
@@ -6,9 +6,6 @@ const db = pgp({
   port: 5432,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS
-  //ssl:{
-    //rejectUnauthorized:false,
-  
+  password: process.env.DB_PASS,
 });
 module.exports = db;
